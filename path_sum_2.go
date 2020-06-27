6a54b0683b961853f8a7e7687cf2e5b21a04f9aa
Aishwarya Pant <aishwaryapant@monzo.com>
2020-06-27 12:10:12 +0100

    make deep copy of nodes

--- path_sum_2.go
+++ path_sum_2.go
@@ -1,59 +1,38 @@
 package main
 
-import "fmt"
-
-/*
- * Definition for a binary tree node.
- */
+// TreeNode is the definition of a binary tree node.
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func pprint(ll *[][]int) {
-	for i := range *ll {
-		fmt.Printf("Node: %v, %p, %p, %v \n", i, ll, (*ll)[i], (*ll)[i])
-	}
-}
-
 func pathSum(root *TreeNode, sum int) [][]int {
 
-	return paths(root, 0, sum, []int{}, [][]int{})
+	allPaths := [][]int{}
+	paths(root, 0, sum, []int{}, &allPaths)
+	return allPaths
 }
 
-func paths(root *TreeNode, partialSum, targetSum int, nodes []int, allPaths [][]int) [][]int {
+func paths(root *TreeNode, partialSum, targetSum int, nodes []int, allPaths *[][]int) {
 	if root == nil {
-		return allPaths
+		return
 	}
 
-	// fmt.Printf("root: %v, partial: %v, target: %v, nodes: %v, allPaths 1: %+v\n", root.Val, partialSum, targetSum, nodes, allPaths)
 	partialSum += root.Val
-	// fmt.Printf("root: %v, partial: %v, target: %v, nodes: %v, allPaths 2: %#v\n", root.Val, partialSum, targetSum, nodes, allPaths)
 
-	fmt.Printf("before append nodes %p %v\n", &nodes, nodes)
-	pprint(&allPaths)
 	nodes = append(nodes, root.Val)
-	fmt.Printf("after append %p, %v \n", &nodes, nodes)
-	pprint(&allPaths)
-
-	// fmt.Printf("root: %v, partial: %v, target: %v, nodes: %v, allPaths 3: %#v\n", root.Val, partialSum, targetSum, nodes, allPaths)
 
 	// Leaf node
 	if root.Right == nil && root.Left == nil {
 		if partialSum == targetSum {
-			fmt.Println("SUM MATCH BEGIN")
-			fmt.Println(nodes)
-			fmt.Println("SUM MATCH END")
-			allPaths = append(allPaths, nodes)
+			*allPaths = append(*allPaths, append([]int{}, nodes...))
 		}
-		return allPaths
+		return
 	}
 
-	allPaths = paths(root.Left, partialSum, targetSum, nodes, allPaths)
-	fmt.Printf("allPaths after left %v\n", allPaths)
-	allPaths = paths(root.Right, partialSum, targetSum, nodes, allPaths)
-	fmt.Printf("allPaths after right %v\n", allPaths)
+	paths(root.Left, partialSum, targetSum, nodes, allPaths)
+	paths(root.Right, partialSum, targetSum, nodes, allPaths)
 
-	return allPaths
+	return
 }
